2a5f728d686ac55793039499f5158c4c8bbe93f5
XiangyiZhang <shawnz000516@outlook.com>
2022-09-29 02:03:45 -0700

    d

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use('/auth', authRoutes);
 app.use('/questions', qRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://shawnz:shawnz123@cluster0.wfeiyw6.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3100;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
